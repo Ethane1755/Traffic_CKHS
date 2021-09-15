f3502f5c3e9463ecae5065c7c4d608aaa1e179dc
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2021-09-15 13:29:22 +0800

    added 107 data

---
+++ Python/test.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('D:/Database/建中/traffic_CKHS/csv/多年pcu比較.csv', encoding='big5')
+
+rows = (df['方向'] == '合計')
+columns = ['108PCU','時間','109PCU','107PCU']
+result = df.loc[rows, columns].head(10)
+result.set_index('時間', inplace=True)
+print(result)
+
+chart = result.plot(title='南海重慶南路口年度交通狀況',  #圖表標題
+                    xlabel='時間',  #x軸說明文字
+                    ylabel='PCU',  #y軸說明文字
+                    legend=True,  # 是否顯示圖例
+                    figsize=(10, 5),  
+                    marker='o',
+                    linestyle='-')  # 圖表大小
+plt.grid(True, ls='-.')
+plt.text(2.5,420,'PCU=小客車單位(換算)')
+plt.show()
