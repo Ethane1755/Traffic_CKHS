b47f7eaddef3784f7c3183c7fec46f06f26c5ab2
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2021-09-15 13:42:33 +0800

    added 106 data

--- Python/test.py
+++ Python/test.py
@@ -2,21 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv('D:/Database/建中/traffic_CKHS/csv/多年pcu比較.csv', encoding='big5')
+df = pd.read_csv('D:/Database/建中/traffic_CKHS/csv/多年pcu比較.csv', encoding='ansi')
 
 rows = (df['方向'] == '合計')
-columns = ['108PCU','時間','109PCU','107PCU']
+columns = ['106PCU','時間','107PCU','108PCU','109PCU']
 result = df.loc[rows, columns].head(10)
 result.set_index('時間', inplace=True)
 print(result)
 
-chart = result.plot(title='南海重慶南路口年度交通狀況',  #圖表標題
+chart = result.plot.bar(title='南海重慶南路口年度交通狀況',  #圖表標題
                     xlabel='時間',  #x軸說明文字
                     ylabel='PCU',  #y軸說明文字
                     legend=True,  # 是否顯示圖例
-                    figsize=(10, 5),  
-                    marker='o',
-                    linestyle='-')  # 圖表大小
-plt.grid(True, ls='-.')
-plt.text(2.5,420,'PCU=小客車單位(換算)')
+                    figsize=(10, 5))  # 圖表大小
+plt.xticks(rotation='horizontal')
+plt.text(0.09,6700,'PCU=小客車單位(換算)')
 plt.show()
