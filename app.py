db0f2406f9bbd72b3e2553f71adb46c72b2ea956
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2021-10-27 14:50:49 +0800

    finish

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
- 
- 
+
 df = pd.read_csv('多年來之比較.csv' , encoding='ansi')
 
-rows = (df['Year'] == 106) & (df['方向'] == '南海路往西') & (df['轉向'] == '左')
-columns = ['時間', 'PCU', '轉向比']
+rows = (df['方向'] == '南海路往西') & (df['轉向'] == '直行')
+columns = ['時間', 'PCU']
 result = df.loc[rows, columns]
 result.set_index('時間', inplace=True)
- 
-chart = result.plot(  #圖表類型
+
+result.plot(kind='bar', #圖表類型
 		    title='test',  #圖表標題
-                    xlabel='時間',  #x軸說明文字
-                    ylabel='PCU',  #y軸說明文字
-                    legend=True,  # 是否顯示圖例
-                    figsize=(10, 5))  # 圖表大小
+            xlabel='時間',  #x軸說明文字
+            ylabel='PCU',  #y軸說明文字
+            legend=True,  # 是否顯示圖例
+            figsize=(10, 5),) # 圖表大小
+plt.xticks(rotation=45)
 plt.show()
