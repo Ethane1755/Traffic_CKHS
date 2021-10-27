b3892ee71113f847668b86964de3e4c23bce2ea3
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2021-10-27 14:13:13 +0800

    eason

--- app.py
+++ app.py
@@ -2,17 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
  
  
-df = pd.read_csv('多年來之比較.csv')
+df = pd.read_csv('多年來之比較.csv' , encoding='ansi')
+
+rows = (df['Year'] == 106) & (df['方向'] == '南海路往西') & (df['轉向'] == '左')
+columns = ['時間', 'PCU', '轉向比']
+result = df.loc[rows, columns]
+result.set_index('時間', inplace=True)
  
-rows = (df['年別'] == 2019) & (df['縣市別'] == '臺北市')
-columns = ['細分', '1月', '2月', '3月']
-result = df.loc[rows, columns].head(100)
-result.set_index('細分', inplace=True)
- 
-chart = result.plot(kind='bar',  #圖表類型
-		    title='2019年臺北市各景點旅客人數',  #圖表標題
-                    xlabel='細分(景點名稱)',  #x軸說明文字
-                    ylabel='人數',  #y軸說明文字
+chart = result.plot(  #圖表類型
+		    title='test',  #圖表標題
+                    xlabel='時間',  #x軸說明文字
+                    ylabel='PCU',  #y軸說明文字
                     legend=True,  # 是否顯示圖例
                     figsize=(10, 5))  # 圖表大小
 plt.show()
